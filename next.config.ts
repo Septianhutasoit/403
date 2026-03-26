25e9fbacedb61929553faf9b8871a76f1f39de7d
Septianhutasoit <septianhts95@gmail.com>
2026-03-26 17:24:51 +0700

    s

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // Hapus eslint dari sini
 };
 
 export default nextConfig;
